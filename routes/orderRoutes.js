3b288bebcf8c31f0da3f7b10667ac5a851b3123d
Gaelle Medawar <gaellemedawarr@gmail.com>
2026-03-21 00:02:58 +0200

    routes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/orderController.js');
+
+router.post('/', orderController.createOrder);
+router.get('/', orderController.getAllOrders);
+router.get('/:id', orderController.getOrderById);
+router.get('/customer/:id', orderController.getCustomerOrders);
+router.put('/:id', orderController.updateOrder);
+router.delete('/:id', orderController.deleteOrder);
+
+module.exports = router;
